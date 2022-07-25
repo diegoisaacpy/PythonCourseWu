659912ec174bb069e43be4b015e5d442f2238e4f
cheems <diegoisaacpy@gmail.com>
2022-07-25 15:04:54 -0400

    Add two digit numbers

---
+++ day2/exercise1.py
@@ -0,0 +1,7 @@
+number = input("Give me a number: ")
+
+first_digit = int(number[0])
+second_digit = int(number[1])
+
+print(f"The sum of {first_digit} + {second_digit} = {first_digit + second_digit}")
+print(first_digit + second_digit)
